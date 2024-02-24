d72c99ac0d9fd7418fa037de364527cf3daa987b
Morty.Pham <trang.phamvuthu@fssc.com.vn>
2024-02-24 22:39:38 +0700

    commit btvn lesson 1

---
+++ NETCORE_LESSON1/LESSON01.MVC/Controllers/DemoController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LESSON01.MVC.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
